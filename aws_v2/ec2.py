d97485c85b40af6c39a9f447ca76824296b566e8
Chris Rodenas <rodenas@gmail.com>
2025-08-24 10:56:42 -0400

    Refactor describe_security_groups function to return a list of SecurityGroup dataclass instances

--- aws_v2/ec2.py
+++ aws_v2/ec2.py
@@ -1,6 +1,16 @@
 """EC2 utility functions for AWS operations."""
 
-from typing import Dict, List, Any
+from typing import List
+from dataclasses import dataclass
+
+
+@dataclass
+class SecurityGroup:
+    GroupId: str
+    GroupName: str
+    Description: str
+    VpcId: str
+
 
 import boto3
 
@@ -11,12 +21,20 @@ client = session.client("ec2")
 
 
 @pivot_exceptions
-def describe_security_groups(ec2_client: boto3.client = None) -> List[Dict[str, Any]]:
+def describe_security_groups(ec2_client: boto3.client = None) -> List[SecurityGroup]:
     """Retrieve all EC2 security groups using pagination."""
     if ec2_client is None:
         ec2_client = client
     results = []
     paginator = ec2_client.get_paginator("describe_security_groups")
     for page in paginator.paginate():
-        results.extend(page["SecurityGroups"])
+        for sg in page["SecurityGroups"]:
+            results.append(
+                SecurityGroup(
+                    GroupId=sg.get("GroupId", ""),
+                    GroupName=sg.get("GroupName", ""),
+                    Description=sg.get("Description", ""),
+                    VpcId=sg.get("VpcId", ""),
+                )
+            )
     return results
